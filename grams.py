aa5e77525cf2bdde5966f3d198d2ce67c4262d24
andcarnivorous <a.favia@tilburguniversity.edu>
2019-09-29 18:15:36 +0000

    Fixed function, added comments

--- grams.py
+++ grams.py
@@ -5,42 +5,48 @@ import pickle
 import argparse
 
 parser = argparse.ArgumentParser()
+#Change the Defaults to your preference.
 parser.add_argument('-f', '--file', default="/home/main/Documents/NLPProject/natives.tok") # CHANGE DEFAULT DIRECTORY TO FILE LOCATION
 parser.add_argument("-p", "--pickle", default=False)
 parser.add_argument("-k", "--keyword", default="plumber")
+parser.add_argument("-g", "--grams", default=2)
 args = parser.parse_args()
 
+final_set = set()
 
-def find_keywordgrams(keyword, _list):
-
+def find_keyword(keyword, _list):
+    # finds a keyword in any output from tokenize or ngrams.
     for x in _list:
         if isinstance(x, list):
             find_keyword(keyword, x)
 
+        elif isinstance(x, str):
+            if keyword in x:
+                final_set.add(tuple(_list))
+
         else:
             for y in x:
                 if keyword in y:
-                    print(_list, "\n\n")
+                    final_set.add(tuple(_list))
 
+    return None
 
 with open("%s" % args.file, "r") as f:
     f = f.readlines()
 
 sentences = [word_tokenize(x) for x in f]
 
-#bigrams = [list(nltk.bigrams(x)) for x in sentences]
-trigrams = [list(nltk.trigrams(x)) for x in sentences]
-#quadragrams = [list(nltk.ngrams(x, 4)) for x in sentences]
+n_grams = [list(nltk.ngrams(x, args.grams)) for x in sentences]
 
-print(find_keywordgrams(args.keyword, trigrams))
+#Define keyword and grams. 
+find_keyword(args.keyword, n_grams)
 
+print(final_set)
 
 if args.pickle == True:
-    with open('bigrams.pickle', 'wb') as handle:
-        pickle.dump(bigrams, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('ngrams.pickle', 'wb') as handle:
+        pickle.dump(n_grams, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    with open('trigrams.pickle', 'wb') as handle:
-        pickle.dump(trigrams, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-    with open('quadragrams.pickle', 'wb') as handle:
-        pickle.dump(quadragrams, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    with open('keywordmatches.pickle', 'w') as handle:
+        pickle.dump(final_set, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    
