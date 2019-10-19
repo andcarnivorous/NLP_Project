e7dd5f6cae3e5ad7c047b051b56b26a0c52ccc50
Anton Stepanov <46137998+stepanov-a-o@users.noreply.github.com>
2019-10-19 11:21:55 +0200

    Update sorter.py

--- sorter.py
+++ sorter.py
@@ -11,10 +11,7 @@ import pandas as pd
 import shutil
 
 os.chdir("2539/")
-
-
 doc = pd.read_excel("download/documentation/BAWE.xls")
-
 directory = os.chdir('download/CORPUS_TXT')
 
 doc.iloc[0][25]
